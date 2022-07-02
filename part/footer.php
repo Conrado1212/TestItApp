f52580d1261cbfdb76ff0059116d2bf29128b0ec
Conrado1212 <conrado1212@gmail.com>
2022-07-02 17:17:35 +0200

    add menu ready

---
+++ part/footer.php
@@ -0,0 +1,9 @@
+<footer>
+        <div class="conteiner">
+        <div class="footer">
+            TestItApp.com &copy; 2022 Thank you for your visit
+        </div>
+       </div>
+    </footer>
+</body>
+</html>
---
+++ part/social.php
@@ -0,0 +1,22 @@
+<section class="Social">
+        <div class="social">
+      
+            <div class="socialdiv">
+                <div class="fb">
+                  <a href=" https://www.facebook.com/"><i class="icon-facebook-squared"></i></a>
+                </div>
+                <div class="yt">
+                    <a href="https://www.youtube.com/"><i class="icon-youtube-play"></i></a>
+                </div>
+                <div class="tw">
+                    <a href="https://www.twitch.tv/"><i class="icon-twitch"></i></a>
+                </div>
+                <div class="am">
+                   <a href="https://www.amazon.de/"><i class="icon-amazon" ></i></a>
+    
+                </div>
+                <div style="clear: both"> </div>
+            </div>
+       
+         </div>
+     </section>
