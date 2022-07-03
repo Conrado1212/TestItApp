0d9fc3bddd580a579dae0cfd37f6a39eaaf7f345
Conrado1212 <conrado1212@gmail.com>
2022-07-03 13:31:11 +0200

    add javascript

---
+++ js/javaGame.js
@@ -0,0 +1,8 @@
+const question = document.getElementById('qestion');
+const choice  = document.getElementById('text');
+
+let currQestion = {};
+let acceptAns =true;
+let score = 0;
+let counterQ = 0;
+let availableQ =[];
